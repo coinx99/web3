04534a693296912bc8bd7b35152916bd46da0e8c
Simon <simon72217@gmail.com>
2023-10-06 01:09:58 +0700

    a

--- test.ts
+++ test.ts
@@ -8,7 +8,7 @@ var chain = connectChain("ethers", CHAINS[5])
 
 chain?.events.on("connected", ({ provider, network }) => {
     chain?.getBalance("0x122348D81f32D0e13ee023B07d400Fd6CF81a6bF").then(balance => {
-        log("balance", Number(balance) / Number(chain?.decimals))
+        log("balance", Number(balance) / Number(chain?.decimals), chain?.symbol)
         // log(chain, CHAINS[chain?.chainId])
     })
 })
